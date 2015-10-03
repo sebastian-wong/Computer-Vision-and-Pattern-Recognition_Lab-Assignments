8415fc3b9eed338c9e49d4d0762806427a0ba47f
Whisperingroad <sebastian.wong@hotmail.com>
2015-10-03 21:35:22 +0800

    Corrected code to suit requirements

--- A0101856J_SebastianWong_Lab6/A0101856J_SebastianWong_Lab6.py
+++ A0101856J_SebastianWong_Lab6/A0101856J_SebastianWong_Lab6.py
@@ -8,17 +8,23 @@ def convertToIntegers(frameWidth, frameHeight, framesPerSecond, frameCount):
     frameWidth = int(frameWidth)
     frameHeight = int(frameHeight)
     framesPerSecond = int(framesPerSecond)
-    frameCount = int(framesCount)
+    frameCount = int(frameCount)
     return frameWidth, frameHeight, framesPerSecond, frameCount
 
-def backgroundExtraction(frameCount):
-    ret,imageFrame = cap.read()
-    avgImageFrame = np.float32(imageFrame)
-    for imgFrame in range (1,frameCount):
-        ret, imageFrame = cap.read()
-        avgImageFrame = avgImageFrame + imageFrame
-    avgImageFrame = avgImageFrame/frameCount
-    return avgImageFrame    
+def backgroundExtraction(cap,frameCount):
+    ret,image = cap.read()
+    avgImage = np.float32(image)
+    normImg = 0
+    for frame in range (1,frameCount):
+        ret, image = cap.read()
+        avgImage = (frame/(frame+1.0))*avgImage + (1.0/(frame+1.0))*image
+        normImg = cv2.convertScaleAbs(avgImage)
+        cv2.imshow('image',image)
+        cv2.imshow('normImg', normImg)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()    
+    cap.release()    
+    return normImg    
            
 # Reading the video
 cap = cv2.VideoCapture(os.getcwd() + "/videos/traffic.mp4")
@@ -27,7 +33,6 @@ frameHeight = cap.get(4)
 framesPerSecond = cap.get(5)
 frameCount = cap.get(7)
 frameWidthInt, frameHeightInt, framesPerSecondInt, frameCountInt = convertToIntegers(frameWidth, frameHeight, framesPerSecond, frameCount)
-background = backgroundExtraction(frameCountInt)
+background = backgroundExtraction(cap,frameCountInt)
 cv2.imwrite(os.getcwd() + "/results/background.jpg", background)
-
 
